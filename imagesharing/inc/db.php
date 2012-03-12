3e448ec08662786d6a19b54504cd29f9039ada4e
onedream87 <onedream87@yahoo.cn>
2012-03-12 14:35:07 +0800

    nothing

--- imagesharing/inc/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db_server = 'localhost';
-$db_name = 'freeimage';
-$db_user = 'root';
-$db_password = '';
-?>
--- imagesharing/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-session_destroy();
-      setcookie("cookname", "", time()-60*60*24*365, "/");
-      setcookie("cookpass", "", time()-60*60*24*365, "/");
-	header("Location: index.php");
-	exit;
-?> 
